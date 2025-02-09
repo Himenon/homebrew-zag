1c83899279ba492e9e58004d6839173528b9bc90
K.Himeno <6715229+Himenon@users.noreply.github.com>
2025-02-09 22:28:50 +0900

    fix: 0.0.8

--- Formula/zag.rb
+++ Formula/zag.rb
@@ -11,13 +11,13 @@ class Zag < Formula
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://github.com/Himenon/zag/releases/download/v#{version}/zag.zip"
-      sha256 "fc2513a3049837318222a638331d5de1becb3b0403ac3f07b0ca629da0856409" # zag-darwin-aarch64.zip
+      url "https://github.com/Himenon/zag/releases/download/v#{version}/zag-macos-aarch64.zip"
+      sha256 "d4feba7f2c971e53639877fba1ad0c17d531b60571264be7bc97e38f35fe173b"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/Himenon/zag/releases/download/zag-v#{version}/zag-linux-aarch64.zip"
-      sha256 "d1dbaa3e9af24549fad92bdbe4fb21fa53302cd048a8f004e85a240984c93d4d" # zag-linux-aarch64.zip
+      sha256 "d1dbaa3e9af24549fad92bdbe4fb21fa53302cd048a8f004e85a240984c93d4d"
     end
   else
     odie "Unsupported platform. Please submit a bug report here: https://zag.sh/issues\n#{OS.report}"
