c3a8e36c40ddf4d0eec09b9c173c88f79fd7f619
K.Himeno <6715229+Himenon@users.noreply.github.com>
2025-02-11 18:16:40 +0900

    feat: update release script (#7)

--- Formula/zag@0.0.8.rb
+++ Formula/zag@0.0.8.rb
@@ -2,7 +2,7 @@ class Zag < Formula
   desc "This library just want to call a library made with Zig lang “Zag.”"
   homepage "https://github.com/Himenon/zag"
   license "MIT"
-  version "0.0.9"
+  version "0.0.8"
 
   livecheck do
     url "https://github.com/Himenon/zag/releases/latest"
@@ -11,13 +11,13 @@ class Zag < Formula
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://github.com/Himenon/zag/releases/download/v#{version}/zag-darwin-aarch64.zip"
-      sha256 "f2c6d931f9fffd0692812ac23b7c00149247385def70c5061858467c464897e5" # zag-darwin-aarch64.zip
+      url "https://github.com/Himenon/zag/releases/download/v#{version}/zag-macos-aarch64.zip"
+      sha256 "d4feba7f2c971e53639877fba1ad0c17d531b60571264be7bc97e38f35fe173b"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/Himenon/zag/releases/download/v#{version}/zag-linux-x86_64.zip"
-      sha256 "ef2a1dcb5659d4ec6d603d396962092208a6cb6031ae1018cc4612537abc2f6d" # zag-linux-x86_64.zip
+      url "https://github.com/Himenon/zag/releases/download/zag-v#{version}/zag-linux-aarch64.zip"
+      sha256 "d1dbaa3e9af24549fad92bdbe4fb21fa53302cd048a8f004e85a240984c93d4d"
     end
   else
     odie "Unsupported platform. Please submit a bug report here: https://zag.sh/issues\n#{OS.report}"
